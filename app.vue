6ee42cbdfd88be677ed75b06f3ec06a54dded9f9
tudorfis <tudor.fis@gmail.com>
2022-07-02 18:37:14 +0300

    changes

--- app.vue
+++ app.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-  enum Gender {
-    GIRL = "Girl",
-    BOY = "Boy",
-    UNISEX = "Unisex",
-  }
+enum Gender {
+  GIRL = "Girl",
+  BOY = "Boy",
+  UNISEX = "Unisex",
+}
 
-  enum Popularity {
-    TRENDY = "Trendy",
-    UNIQUE = "Unique",
-  }
+enum Popularity {
+  TRENDY = "Trendy",
+  UNIQUE = "Unique",
+}
 
-  enum Length {
-    SHORT = "Short",
-    LONG = "Long",
-    ALL = "All",
-  }
+enum Length {
+  SHORT = "Short",
+  LONG = "Long",
+  ALL = "All",
+}
 
-  interface OptionsState {
-    gender: Gender;
-    popularity: Popularity;
-    length: Length;
-  }
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
 
-  const options = reactive<OptionsState>({
-    gender: Gender.GIRL,
-    length: Length.SHORT,
-    popularity: Popularity.TRENDY,
-  })
+const options = reactive<OptionsState>({
+  gender: Gender.GIRL,
+  length: Length.SHORT,
+  popularity: Popularity.TRENDY,
+});
 </script>
 
 <template>
@@ -40,18 +40,21 @@
           <button
             class="option option-left"
             :class="options.gender === Gender.BOY && 'option-active'"
+            @click="options.gender = Gender.BOY"
           >
             Boy
           </button>
           <button
             class="option"
             :class="options.gender === Gender.UNISEX && 'option-active'"
+            @click="options.gender = Gender.UNISEX"
           >
             Unisex
           </button>
           <button
             class="option option-right"
             :class="options.gender === Gender.GIRL && 'option-active'"
+            @click="options.gender = Gender.GIRL"
           >
             Girl
           </button>
@@ -63,35 +66,40 @@
           <button
             class="option option-left"
             :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="options.popularity = Popularity.TRENDY"
           >
             Trendy
           </button>
           <button
             class="option option-right"
             :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="options.popularity = Popularity.UNIQUE"
           >
             Unique
           </button>
         </div>
       </div>
       <div class="option-container">
-        <h4>2) Choose name's length</h4>
+        <h4>3) Choose name's length</h4>
         <div class="option-buttons">
           <button
             class="option option-left"
             :class="options.length === Length.LONG && 'option-active'"
+            @click="options.length = Length.LONG"
           >
             Long
           </button>
           <button
             class="option"
             :class="options.length === Length.ALL && 'option-active'"
+            @click="options.length = Length.ALL"
           >
             All
           </button>
           <button
             class="option option-right"
             :class="options.length === Length.SHORT && 'option-active'"
+            @click="options.length = Length.SHORT"
           >
             Short
           </button>
